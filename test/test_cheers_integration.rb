e068805d91e1fcca831321e3b03f0f90e0ff7bfa
thedean11 <edean1010@gmail.com>
2015-04-21 13:29:01 -0500

    passing no name test

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -3,6 +3,11 @@ require 'minitest/autorun'
 class TestCheersIntegration < MiniTest::Test
 
 	def test_that_the_tests_run
-		assert_equal true,false
+		output = `./cheers`
+		expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers [Name] [MM/DD Birthday]`
+EOS
+		assert_equal expected, output
 	end
 end
